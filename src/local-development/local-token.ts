dc1e8684e7017416f3334da313244837d2b7e430
Konrad Hopek <konrad.hopek@ge.com>
2019-02-23 18:23:37 +0100

    add local token for production build

---
+++ src/local-development/local-token.ts
@@ -0,0 +1,8 @@
+
+/**
+ * IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT!
+ * THIS FILE HAS TOKEN FOR LOCAL DEVELOPMENT PURPOSE.
+ */
+/* tslint:disable */
+export const localToken = 'empty_token';
+/* tslint:enable */
